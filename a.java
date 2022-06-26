06d1357a5a9dcd0124422576108b2b7a7cea162e
PRASHANT SACHAN <69834249+prashant2150487@users.noreply.github.com>
2022-06-26 15:25:17 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,27 @@
+
+class Node {
+    int key;
+    Node left;
+    Node right;
+
+    public Node(int item) {
+        key = item;
+        left = right = null;
+    }
+}
+
+class a {
+    Node root;
+
+    a(int key) {
+        root = new Node(key);
+    }
+
+    a() {
+        root = null;
+    }
+
+public static void main(String[] args) {
+    A tree=new A();
+    tree.root()
+}
